c7c0512c853b413849649c22c16f5d1c5eca88f1
Denise Bosisio <105015678+denisebosisio@users.noreply.github.com>
2022-07-25 08:20:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export default class App extends Component {
+  state = {
+    num: 0
+  };
+
+  incremento = () => {
+    if (this.state.num < 10) {
+      this.setState({
+        num: this.state.num + 1
+      });
+    }
+  };
+
+  decremento = () => {
+    if (this.state.num > 0) {
+      this.setState({
+        num: this.state.num - 1
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>{this.state.num}</h2>
+        <button onClick={this.incremento}>+</button>
+      </div>
+    );
+  }
+}
